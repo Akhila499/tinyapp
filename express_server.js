1692e0c1338acedd25e29678e41af128f3de792a
Akhila499 <akhilathota499@gmail.com>
2021-08-02 17:34:42 -0400

    small changes

--- express_server.js
+++ express_server.js
@@ -8,13 +8,16 @@ const urlDatabase = {
 };
 
 app.get("/", (req, res) => {
+  //const a = 1;
+  //res.send(`a = ${a}`)
   res.send("Hello!");
 });
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+ res.send("<html><body>Hello <b>World</b></body></html>\n");
+ //res.send(`a = ${a}`);
 });
 
 app.listen(PORT, () => {
